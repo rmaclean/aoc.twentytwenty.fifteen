e4df0bc6dfc57476e4f2d9b7aae3410d9a05f01d
Robert MacLean <robert@sadev.co.za>
2020-12-16 11:22:00 +0200

    part 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.IO;
+
+var data = (await File.ReadAllTextAsync("data.txt")).Split(',').Select(c => Convert.ToInt32(c)).ToList();
+var initialSet = data.Count;
+var initialIndex = 0;
+var numbers = data.ToDictionary(n => n, n => ++initialIndex);
+
+for (var index = initialSet; index < 2020; index++)
+{
+    var number = data[index - 1];
+
+    var seenAtleastOnce = numbers.ContainsKey(number);
+    if (!seenAtleastOnce)
+    {
+        numbers[number] = index;
+        data.Add(0);
+    }
+    else
+    {
+        var last = numbers[number];
+        var next = index - last;
+        numbers[number] = index;
+        data.Add(next);
+    }
+}
+
+Console.WriteLine(data.Aggregate("", (curr, next) => curr += next.ToString() + ", "));
+Console.WriteLine(data.Last());
+
